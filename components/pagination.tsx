d3b302e4a1592dca010f6f12cf83182e9e2be293
Dulnyarat9458 <dulnyarat.9458@gmail.com>
2024-03-07 22:20:04 +0700

    make pagination may complete

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -25,26 +25,84 @@ export function PaginationMain({ paginationValue }: any) {
       <PaginationContent>
         {
           paginationValue.page !== 1 && (
-            <PaginationItem>
-              <PaginationPrevious href={createPageURL(paginationValue.page - 1)} />
-            </PaginationItem>
+            <>
+              <PaginationItem>
+                <PaginationPrevious href={createPageURL(1)} />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationPrevious href={createPageURL(paginationValue.page - 1)} />
+              </PaginationItem>
+            </>
+
           )
         }
-        {[...Array(paginationValue.pageCount)].map((_, index: number) =>
-          <PaginationItem key={index}>
-            {
-              index + 1 === paginationValue.page ? (
-                <PaginationLink href={createPageURL(index + 1)} isActive>
-                  {index + 1}
-                </PaginationLink>
-              ) : (
-                <PaginationLink href={createPageURL(index + 1)}>
-                  {index + 1}
-                </PaginationLink>
-              )
-            }
-          </PaginationItem>
-        )}
+
+        {
+          paginationValue.pageCount <= 5 ?
+            [...Array(paginationValue.pageCount)].map((_, index: number) =>
+              <PaginationItem key={index}>
+                {
+                  index + 1 === paginationValue.page ? (
+                    <PaginationLink href={createPageURL(index + 1)} isActive>
+                      {index + 1}
+                    </PaginationLink>
+                  ) : (
+                    <PaginationLink href={createPageURL(index + 1)}>
+                      {index + 1}
+                    </PaginationLink>
+                  )
+                }
+              </PaginationItem>
+            )
+            : (paginationValue.page >= 4 && paginationValue.pageCount - paginationValue.page > 2)
+              ? [...Array(5)].map((_, index: number) =>
+                <PaginationItem key={index}>
+                  {
+                    index + 1 - 2 + paginationValue.page === paginationValue.page ? (
+                      <PaginationLink href={createPageURL(index + 1 - 2 + paginationValue.page)} isActive>
+                        {index + 1 - 2 + paginationValue.page}
+                      </PaginationLink>
+                    ) : (
+                      <PaginationLink href={createPageURL(index + 1 - 2 + paginationValue.page)}>
+                        {index + 1 - 2 + paginationValue.page}
+                      </PaginationLink>
+                    )
+                  }
+                </PaginationItem>
+              ) : paginationValue.page <= 3
+                ?
+                [...Array(5)].map((_, index: number) =>
+                  <PaginationItem key={index}>
+                    {
+                      index + 1 === paginationValue.page ? (
+                        <PaginationLink href={createPageURL(index + 1)} isActive>
+                          {index + 1}
+                        </PaginationLink>
+                      ) : (
+                        <PaginationLink href={createPageURL(index + 1)}>
+                          {index + 1}
+                        </PaginationLink>
+                      )
+                    }
+                  </PaginationItem>
+                )
+                :
+                [...Array(5)].map((_, index: number) =>
+                  <PaginationItem key={index}>
+                    {
+                      index + 1 + (paginationValue.pageCount - 5) === paginationValue.page ? (
+                        <PaginationLink href={createPageURL(index + 1 + (paginationValue.pageCount - 5))} isActive>
+                          {index + 1 + (paginationValue.pageCount - 5)}
+                        </PaginationLink>
+                      ) : (
+                        <PaginationLink href={createPageURL(index + 1 + (paginationValue.pageCount - 5))}>
+                          {index + 1 + (paginationValue.pageCount - 5)}
+                        </PaginationLink>
+                      )
+                    }
+                  </PaginationItem>
+                )
+        }
         {
           paginationValue.page !== paginationValue.pageCount && (
             <PaginationItem>

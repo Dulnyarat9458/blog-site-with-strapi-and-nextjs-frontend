cb570849eaab75bdecb33a61a5846c4298ab42a1
Dulnyarat9458 <dulnyarat.9458@gmail.com>
2024-03-22 00:14:04 +0700

    fix broken style

--- app/tags/[slug]/page.tsx
+++ app/tags/[slug]/page.tsx
@@ -25,6 +25,7 @@ interface ContentAttributes {
     }
   },
   name: string;
+  categories: any;
 }
 
 interface Content {
@@ -39,7 +40,7 @@ async function getData(slug: string) {
       'Authorization': `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
     },
   };
-  const url = `${process.env.NEXT_PUBLIC_API_URL}/api/tags/${slug}?populate[contents][populate][cover]=*`
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/api/tags/${slug}?populate[contents][populate]=*`
   const res = await fetch(url, options);
   if (!res.ok) {
     throw new Error('That content can’t be found.')
@@ -59,23 +60,29 @@ export default async function TagsPage(props: Props) {
               (content: Content) => {
                 return (
                   <Link href={"/contents/" + content.id}>
-                    <Card className="h-full border-border duration-200 hover:border-primary hover:text-primary">
+                    <Card className="h-full border-border duration-200 hover:border-primary hover:text-primary group flex flex-col">
                       <CardHeader className="p-0">
-                        <Image
-                          src={process.env.NEXT_PUBLIC_API_URL + "" + content.attributes.cover.data.attributes.url}
-                          layout="responsive"
-                          alt={content.attributes.cover.data.attributes.alternativeText}
-                          className="w-full aspect-square object-cover rounded-t-lg"
-                          width={1200}
-                          height={1200}
-                        />
+                      <div className="aspect-square w-full h-auto overflow-hidden rounded-t-lg">
+                          <Image
+                            src={process.env.NEXT_PUBLIC_API_URL + "" + content.attributes.cover.data.attributes.url}
+                            layout="responsive"
+                            alt={content.attributes.cover.data.attributes.alternativeText}
+                            className="w-full aspect-square object-cover rounded-t-lg duration-300 transition group-hover:scale-110"
+                            width={1200}
+                            height={1200}
+                          />
+                        </div>
                       </CardHeader>
-                      <CardContent className="p-4">
-                        <CardTitle className="text-xl">{content.attributes.name}</CardTitle>
-                        <CardDescription></CardDescription>
+                      <CardContent className="px-4 pt-4 pb-6 flex-1 relative">
+                        <CardTitle className="text-xl mb-1">{content.attributes.name}</CardTitle>
+                        {
+                          content.attributes.categories.data.map((category: any, index: number) => (
+                            <div className="inline mr-2 whitespace-nowrap truncate">{category.attributes.name}{content.attributes.categories.data.length - 1 === index ? "" : ", "}</div>
+                          ))
+                        }
+                        <div className="mt-2 bottom-0 h-2 z-40"></div>
+                        <div className="h-1 w-0 group-hover:w-[calc(100%-32px)] bg-primary duration-300 absolute bottom-4"></div>
                       </CardContent>
-                      <CardFooter>
-                      </CardFooter>
                     </Card>
                   </Link>
                 )

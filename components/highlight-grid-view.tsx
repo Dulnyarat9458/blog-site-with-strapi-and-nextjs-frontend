6986a6916d30147d1616e67518b015f43c8631c8
Dulnyarat9458 <dulnyarat.9458@gmail.com>
2024-03-22 14:01:32 +0700

    change interface structure

--- components/highlight-grid-view.tsx
+++ components/highlight-grid-view.tsx
@@ -3,54 +3,55 @@ import Image from "next/image";
 import Link from "next/link";
 
 
-interface ContentAttribute {
-  content: {
-    data: {
-      id: number,
+interface HighlightGridViewProps {
+  contentData: {
+    data: Array<{
+      id: number;
       attributes: {
-        cover: {
+        content: {
           data: {
+            id: number,
             attributes: {
-              url: string,
-              alternativeText: string
+              cover: {
+                data: {
+                  attributes: {
+                    url: string,
+                    alternativeText: string
+                  }
+                }
+              }
+              categories: {
+                data: Array<{
+                  id: number;
+                  attributes: {
+                    name: string
+                  }
+                }>
+              },
+              name: string
             }
           }
         }
-        categories: any,
-        name: string
-      }
-    }
-  }
+      };
+    }>;
+  };
 }
 
-interface Ctn {
-  id: number,
-  attributes: ContentAttribute;
-}
-
-interface ContentData {
-  data: Ctn[];
-}
-
-interface HighlightGridViewProps {
-  contentData: ContentData;
-}
 
 export function HighlightGridView(props: HighlightGridViewProps) {
   const { contentData } = props;
-  console.log(contentData.data[0].attributes.content.data.attributes)
   return (
     <div className="w-full grid cols-2 lg:grid-cols-4 gap-4">
       {
-        contentData.data.map((ctn: Ctn, index: number) => (
+        contentData.data.map((content, index) => (
           <div className={`relative aspect-square ${(index == 0) ? 'col-span-2 row-span-2' : 'col-span-2 row-span-2  sm:col-span-1 sm:row-span-1 '}`}>
-            <Link href={"contents/" + ctn.attributes.content.data.id} className="group point-cursor">
+            <Link href={"contents/" + content.attributes.content.data.id} className="group point-cursor">
               <div className="overflow-hidden object-cover object-center h-full w-full">
                 <Image
-                  src={process.env.NEXT_PUBLIC_API_URL + "" + ctn.attributes.content.data.attributes.cover.data.attributes.url}
+                  src={process.env.NEXT_PUBLIC_API_URL + "" + content.attributes.content.data.attributes.cover.data.attributes.url}
                   width={1200}
                   height={1200}
-                  alt={ctn.attributes.content.data.attributes.cover.data.attributes.alternativeText}
+                  alt={content.attributes.content.data.attributes.cover.data.attributes.alternativeText}
                   className="transition group-hover:scale-110 object-cover object-center h-full w-full duration-300"
                 />
               </div>
@@ -58,12 +59,12 @@ export function HighlightGridView(props: HighlightGridViewProps) {
                 className="duration-300 w-full h-full flex justify-center items-end absolute bottom-0 text-center text-xl font-semibold p-4 
                 bg-gradient-to-t
               from-black/70 from-20% via-black/30 via-70% to-black/0 transition-all hover:bg-black/50 ">
-                <div className="text-white duration-300 transition-all absolute bottom-4 group-hover:bottom-1/2 group-hover:translate-y-1/2 group-hover:text-primary p-1">
-                  "{ctn.attributes.content.data.attributes.name}"
+                <div className="text-white duration-300 transition-all absolute bottom-8 group-hover:bottom-1/2 group-hover:translate-y-1/2 group-hover:text-primary p-1">
+                  "{content.attributes.content.data.attributes.name}"
                   {
                     <div className="whitespace-nowrap truncate opacity-0 group-hover:opacity-100 duration-200 text-primary">
-                      {ctn.attributes.content.data.attributes.categories.data.map((category: any, index: number) => (
-                        <div className="inline text-sm mr-2 whitespace-nowrap truncate">{category.attributes.name}{ctn.attributes.content.data.attributes.categories.data.length - 1 === index ? "" : ", "}</div>
+                      {content.attributes.content.data.attributes.categories.data.map((category, index) => (
+                        <div className="inline text-sm mr-2 whitespace-nowrap truncate">{category.attributes.name}{content.attributes.content.data.attributes.categories.data.length - 1 === index ? "" : ", "}</div>
                       ))
                       }
                     </div>

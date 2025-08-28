540fdac1d309fdb673c8c7ed63f5367436669936
Dulnyarat9458 <dulnyarat.9458@gmail.com>
2025-08-28 17:07:43 +0700

    fix

--- components/highlight-grid-view.tsx
+++ components/highlight-grid-view.tsx
@@ -54,17 +54,16 @@ export function HighlightGridView(props: HighlightGridViewProps) {
         contentData.data.map((content, index) => {
           const coverData = content?.attributes?.content?.data?.attributes?.cover?.data?.attributes?.url;
 
-
           return (
             <div key={index} className={`relative aspect-square ${(index == 0) ? 'col-span-2 row-span-2' : 'col-span-2 row-span-2  sm:col-span-1 sm:row-span-1 '}`}>
-              <Link href={"contents/" + content.attributes.content.data.id} className="group point-cursor">
+              <Link href={"contents/" + content?.attributes?.content?.data?.id} className="group point-cursor">
                 <div className="overflow-hidden object-cover object-center h-full w-full">
                   {
                     coverData ? <Image
                       src={process.env.NEXT_PUBLIC_API_URL + "" + coverData}
                       width={1200}
                       height={1200}
-                      alt={content.attributes.content.data.attributes.cover.data.attributes.alternativeText}
+                      alt={content?.attributes?.content?.data?.attributes?.cover?.data?.attributes?.alternativeText}
                       className="transition group-hover:scale-110 object-cover object-center h-full w-full duration-300"
                       priority={true}
                       placeholder='empty'
@@ -77,10 +76,10 @@ export function HighlightGridView(props: HighlightGridViewProps) {
                 bg-gradient-to-t
               from-black/70 from-20% via-black/30 via-70% to-black/0 transition-all hover:bg-black/50 ">
                   <div className="text-white duration-300 transition-all absolute bottom-8 group-hover:bottom-1/2 group-hover:translate-y-1/2 group-hover:text-primary p-1">
-                    &quot;{content.attributes.content.data.attributes.name}&quot;
+                    &quot;{content?.attributes?.content?.data?.attributes?.name}&quot;
                     {
                       <div className="whitespace-nowrap truncate opacity-0 group-hover:opacity-100 duration-200 text-primary">
-                        {content.attributes.content.data.attributes.categories.data.map((category, index) => (
+                        {content?.attributes?.content?.data?.attributes?.categories?.data.map((category, index) => (
                           <div key={index} className="inline text-sm whitespace-nowrap truncate">{category.attributes.name}{content.attributes.content.data.attributes.categories.data.length - 1 === index ? "" : ", "}</div>
                         ))
                         }
